00b2a6f850ef036725edb6f0f499e5b3ff702ccc
10sr <8.slashes@gmail.com>
2019-02-06 11:34:27 +0900

    Update field type

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.util import timezone
-
+[
 
 class KeyValue(models.Model):
     # TODO: Add user attribute
     created_at = models.DateTimeField(
         "Time when record inserted", default=timezone.now()
     )
-    key = models.CharField(max_length=64, unique=True)
+    key = models.SlugField(unique=True)
     value = models.TextField()
